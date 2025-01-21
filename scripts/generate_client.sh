c4dbcc44ca90c45d3c73a77214583dbb13da4c14
Corentin Merot <cmerot@themarqueeblink.com>
2025-01-21 09:06:15 +0100

    chore: better messages for ./scripts/generate_client.sh

--- scripts/generate_client.sh
+++ scripts/generate_client.sh
@@ -1,16 +1,16 @@
 #! /usr/bin/env bash
 
 set -e
-set -x
 
 rm -rf frontend/src/lib/backend/client frontend/src/lib/backend/openapi.json
 mkdir -p frontend/src/lib/backend
 
 cd backend
+echo "⏳ Generating openapi.json from backend (FastAPI)"
 python -c "import app.main; import json; print(json.dumps(app.main.app.openapi()))" > ../frontend/src/lib/backend/openapi.json
 cd ..
 
 cd frontend
+echo "⏳ Generating TypeScript client from frontend (@hey-api/openapi-ts)"
 pnpm run generate-client
-pnpm run format
 cd ..
